8bd2c302e293bc81b6990eadae2e2e9852602ad4
María <113915017+mariaudc@users.noreply.github.com>
2025-02-22 01:03:20 +0100

    Add files via upload

---
+++ CompetenciApp/src/main/java/com/example/CompetenciApp/CompetenciAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.CompetenciApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompetenciAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompetenciAppApplication.class, args);
+	}
+
+}
---
+++ CompetenciApp/src/test/java/com/example/CompetenciApp/CompetenciAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CompetenciApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompetenciAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
